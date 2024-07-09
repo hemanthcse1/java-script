72fc27f89aa6d9d4704adb0d425a505d9ecd813e
hemanthcse1 <hemanthcse1@gmail.com>
2024-07-09 17:17:44 +0530

    counter function

--- index.js
+++ index.js
@@ -37,3 +37,30 @@
 
 //   document.getElementById("myh3").textContent = circumference;
 // };
+
+/*
+    counter function
+*/
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+
+const countLable = document.getElementById("countLabel");
+
+let count = 0;
+
+increaseBtn.onclick = function () {
+  count++;
+  countLable.textContent = count;
+};
+
+decreaseBtn.onclick = function () {
+  count--;
+  countLable.textContent = count;
+};
+
+resetBtn.onclick = function () {
+  count = 0;
+  countLable.textContent = count;
+};
