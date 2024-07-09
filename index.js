8f208aea6d82b8bf87027dddf7e468495465740b
hemanthcse1 <hemanthcse1@gmail.com>
2024-07-09 17:04:45 +0530

    playing around variables and constants

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
+/*
+    Type conversion
+*/
 
+// let age = window.prompt("How old are you?");
+// age = Number(age);
+// age+=1
+// console.log(age,typeof age);
 
+// let x = "pizza";
+// let y = "pizza";
+// let z = "pizza";
 
-document.getElementById("heading").textContent = `First Heading test`;
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+/*
+    const = a variable that can't be changed
+*/
+
+// const pi = 3.14159;
+// let radius;
+// let circumference;
+
+// radius = window.prompt("Enter the radius of a circle");
+// radius = Number(radius);
+
+// document.getElementById("mysubmit").onclick = function () {
+//   radius = document.getElementById("mytext").value;
+//   radius = Number(radius);
+//   circumference = 2 * pi * radius;
+
+//   document.getElementById("myh3").textContent = circumference;
+// };
